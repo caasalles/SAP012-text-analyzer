2a74366343e15e060f6cde1b0940512c7420f0c2
Carolina Salles <anasallesfotografia@gmail.com>
2023-12-14 11:47:10 -0300

    update

--- src/analyzer.js
+++ src/analyzer.js
@@ -1,21 +1,27 @@
-const analyzer = {  
+const analyzer = {
   getWordCount: (text) => {
-    //TODO: esta función debe retornar el recuento de palabras que se encuentran en el parámetro `text` de tipo `string`.
+    const words = text.trim().split(/\s+/).filter(Boolean);
+    return words.length;
   },
   getCharacterCount: (text) => {
-    //TODO: esta función debe retornar el recuento de caracteres que se encuentran en el parámetro `text` de tipo `string`.
+    return text.length;
   },
   getCharacterCountExcludingSpaces: (text) => {
-    //TODO: esta función debe retornar el recuento de caracteres excluyendo espacios y signos de puntuación que se encuentran en el parámetro `text` de tipo `string`.
+    const cleanText = text.replace(/[^\w\s]/g, '').replace(/\s+/g, '');
+    return cleanText.length;
   },
-  getAverageWordLength: (text) => {    
-    //TODO: esta función debe retornar la longitud media de palabras que se encuentran en el parámetro `text` de tipo `string`.
+  getAverageWordLength: (text) => {
+    const words = text.trim().split(/\s+/).filter(Boolean);
+    const totalWordLength = words.reduce((acc, word) => acc + word.length, 0);
+    return words.length > 0 ? totalWordLength / words.length : 0;
   },
   getNumberCount: (text) => {
-    //TODO: esta función debe retornar cúantos números se encuentran en el parámetro `text` de tipo `string`.
+    const numbers = text.match(/\d+/g);
+    return numbers ? numbers.length : 0;
   },
   getNumberSum: (text) => {
-    //TODO: esta función debe retornar la suma de todos los números que se encuentran en el parámetro `text` de tipo `string`.
+    const numbers = text.match(/\d+/g);
+    return numbers ? numbers.reduce((acc, num) => acc + parseInt(num), 0) : 0;
   },
 };
 
--- src/index.js
+++ src/index.js
@@ -1,3 +1,29 @@
 import analyzer from './analyzer.js';
 
-//TODO: escuchar eventos del DOM e invocar  los métodos del objeto `analyzer`
+let metricas = (text) => {
+    const wordCount = analyzer.getWordCount(text);
+    const characterCount = analyzer.getCharacterCount(text);
+    const averageWordLength = analyzer.getAverageWordLength(text);
+    const numberCount = analyzer.getNumberCount(text);
+    const numberSum = analyzer.getNumberSum(text);
+    const characterCountExcludingSpaces = analyzer.getCharacterCountExcludingSpaces(text);
+
+    document.querySelector('[data-testid="word-count"]').innerText = `Contagem de Palavras: ${wordCount}`;
+    document.querySelector('[data-testid="character-count"]').innerText = `Contagem de Caracteres: ${characterCount}`;
+    document.querySelector('[data-testid="character-no-spaces-count"]').innerText = `Contagem Sem Espaços/Pontuações: ${characterCountExcludingSpaces}`;
+    document.querySelector('[data-testid="number-count"]').innerText = `Contagem de Números: ${numberCount}`;
+    document.querySelector('[data-testid="number-sum"]').innerText = `Soma Total dos Números: ${numberSum}`;
+    document.querySelector('[data-testid="word-length-average"]').innerText = `Comprimento Médio das Palavras: ${averageWordLength.toFixed(2)}`;
+};
+
+let textarea = document.querySelector('textarea[name="user-input"]');
+textarea.addEventListener('input', () => {
+    const text = textarea.value;
+    metricas(text);
+});
+
+let button = document.getElementById('reset-button');
+button.addEventListener('click', () => {
+    textarea.value = '';
+    metricas('');
+});
