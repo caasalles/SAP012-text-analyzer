2ab991d62621e187e33a123981f22f4e807f64cc
Carolina Salles <anasallesfotografia@gmail.com>
2023-12-19 17:47:51 -0300

    projetofinal

--- src/analyzer.js
+++ src/analyzer.js
@@ -8,12 +8,21 @@ const analyzer = {
   },
   getCharacterCountExcludingSpaces: (text) => {
     const cleanText = text.replace(/[^\w\s]/g, '').replace(/\s+/g, '');
+    let noSpaceCount = 0;
+    for(let i = 0; i < cleanText.length; i++) {
+      if (cleanText[i] !== '') {
+        noSpaceCount++;
+      }
+    }
     return cleanText.length;
   },
   getAverageWordLength: (text) => {
     const words = text.trim().split(/\s+/).filter(Boolean);
+    if(words.length === 0) {
+    return 0;
+    }
     const totalWordLength = words.reduce((acc, word) => acc + word.length, 0);
-    return words.length > 0 ? totalWordLength / words.length : 0;
+    return totalWordLength / words.length
   },
   getNumberCount: (text) => {
     const numbers = text.match(/\d+/g);
